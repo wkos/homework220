0270b7587717549e715bafc27e73c6c6685c3b3e
wkos <obohp@wp.pl>
2020-08-19 09:05:09 +0200

    Created empty project Products

---
+++ src/main/java/pl/wkos/homework220/Homework220Application.java
@@ -0,0 +1,13 @@
+package pl.wkos.homework220;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework220Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework220Application.class, args);
+    }
+
+}
